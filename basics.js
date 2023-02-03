ec007bac2d12e2da5714bade0087145f46e75822
DT0998 <duongkytrieuvn@gmail.com>
2023-02-03 17:01:23 +0700

    feat: add smth

--- basics.js
+++ basics.js
@@ -1,18 +1,7 @@
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
-function add(num1, num2, showResult, phrase) {
-    const result = num1 + num2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return num1 + num2;
-    }
-}
 let number1;
 number1 = 5;
 const number2 = 2.8;
-const printResult = true;
 let resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
--- basics.ts
+++ basics.ts
@@ -2,14 +2,14 @@ const button = document.querySelector("button")! as HTMLButtonElement;
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number, showResult: boolean, phrase: string) {
-  const result = num1 + num2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return num1 + num2;
-  }
-}
+// function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+//   const result = num1 + num2;
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return num1 + num2;
+//   }
+// }
 
 // button.addEventListener("click", function () {
 //   console.log(add(+input1.value, +input2.value));
@@ -18,9 +18,9 @@ function add(num1: number, num2: number, showResult: boolean, phrase: string) {
 let number1: number;
 number1 = 5; //5.0
 const number2 = 2.8;
-const printResult = true;
+// const printResult = true;
 let resultPhrase = "Result is: ";
 
-add(number1, number2, printResult, resultPhrase);
+// add(number1, number2, printResult, resultPhrase);
 
 // type typeScript
