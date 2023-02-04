4ea578939cc4d8d5b1fc9f51749d5b144d984da3
DT0998 <duongkytrieuvn@gmail.com>
2023-02-04 21:38:07 +0700

    feat: add smth

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let userInput;
 let userName;
 userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
+userInput = "Max";
+if (typeof userInput === "string") {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occurred!", 500);
--- app.ts
+++ app.ts
@@ -2,7 +2,12 @@ let userInput: unknown;
 let userName: string;
 
 userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') { 
-    userName = userInput; // Error
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+generateError("An error occurred!", 500);
